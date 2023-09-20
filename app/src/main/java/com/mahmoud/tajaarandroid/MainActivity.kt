27e0fc4812fd91516629af3232d8ce9afabcf1d1
Mahmoud <mahmoud.alkateb22@gmail.com>
2023-09-20 06:21:13 -0700

    using nested graph to isolate each part of the app, to easily maintain navigation transitions and back stack entries.

--- app/src/main/java/com/mahmoud/tajaarandroid/MainActivity.kt
+++ app/src/main/java/com/mahmoud/tajaarandroid/MainActivity.kt
@@ -8,17 +8,27 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
 import com.mahmoud.tajaarandroid.presentation.onboarding.OnBoardingStepOne
 import com.mahmoud.tajaarandroid.presentation.onboarding.OnBoardingStepThree
 import com.mahmoud.tajaarandroid.presentation.onboarding.OnBoardingStepTwo
 import com.mahmoud.tajaarandroid.presentation.onboarding.OnBoardingViewModel
 import com.mahmoud.tajaarandroid.presentation.splash.SplashScreen
+import com.mahmoud.tajaarandroid.presentation.util.Route.AUTH
+import com.mahmoud.tajaarandroid.presentation.util.Route.GET_STARTED
+import com.mahmoud.tajaarandroid.presentation.util.Route.ONBOARDING_GRAPH
 import com.mahmoud.tajaarandroid.presentation.util.Route.ONBOARDING_STEP_1
 import com.mahmoud.tajaarandroid.presentation.util.Route.ONBOARDING_STEP_2
 import com.mahmoud.tajaarandroid.presentation.util.Route.ONBOARDING_STEP_3
@@ -33,22 +43,26 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val viewModel = hiltViewModel<OnBoardingViewModel>()
             val navController = rememberNavController()
             TajAarAndroidTheme {
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
                 ) { innerPadding ->
-                    NavHost(navController = navController, startDestination = ONBOARDING_STEP_1 ) {
-                        composable(SPLASH) {
-                            SplashScreen(
-                                modifier = Modifier.padding(innerPadding),
-                                onNavigate =  {
-                                    navController.navigate(ONBOARDING_STEP_1)
-                                }
-                            )
-                        }
-                        composable(ONBOARDING_STEP_1) {
+                    NavHost(navController = navController, startDestination = ONBOARDING_GRAPH ) {
+
+                        navigation(route = ONBOARDING_GRAPH, startDestination = SPLASH) {
+
+                            composable(SPLASH) {
+
+                                SplashScreen(
+                                    modifier = Modifier.padding(innerPadding),
+                                    onNavigate =  {
+                                        navController.navigate(ONBOARDING_STEP_1)
+                                    }
+                                )
+                            }
+                            composable(ONBOARDING_STEP_1) {
+                                val viewModel = it.sharedViewModel<OnBoardingViewModel>(navController = navController)
                                 OnBoardingStepOne(
                                     onNavigate = {
                                         viewModel.nextStep()
@@ -56,21 +70,35 @@ class MainActivity : ComponentActivity() {
                                     },
                                     viewModel = viewModel
                                 )
+                            }
+                            composable(ONBOARDING_STEP_2) {
+                                val viewModel = it.sharedViewModel<OnBoardingViewModel>(navController = navController)
+                                OnBoardingStepTwo(
+                                    viewModel = viewModel,
+                                    onNavigate = {
+                                        viewModel.nextStep()
+                                        navController.navigate(ONBOARDING_STEP_3)
+                                    }
+                                )
+                            }
+                            composable(ONBOARDING_STEP_3) {
+                                val viewModel = it.sharedViewModel<OnBoardingViewModel>(navController = navController)
+                                OnBoardingStepThree(
+                                    viewModel = viewModel,
+                                    onNavigate = {
+                                        navController.navigate(GET_STARTED) {
+                                            popUpTo(SPLASH) {
+                                                inclusive = true
+                                            }
+                                        }
+                                    }
+                                )
+                            }
                         }
-                        composable(ONBOARDING_STEP_2) {
-                            OnBoardingStepTwo(
-                                viewModel = viewModel,
-                                onNavigate = {
-                                    viewModel.nextStep()
-                                    navController.navigate(ONBOARDING_STEP_3)
-                                }
-                            )
-                        }
-                        composable(ONBOARDING_STEP_3) {
-                            OnBoardingStepThree(
-                                viewModel = viewModel,
-                                onNavigate = {}
-                            )
+                        navigation(route = AUTH, startDestination = GET_STARTED) {
+                            composable(GET_STARTED) {
+                                Text(text = "Hi from get started!")
+                            }
                         }
                     }
 
@@ -78,5 +106,15 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+
+@Composable
+inline fun <reified T : ViewModel> NavBackStackEntry.sharedViewModel(navController: NavController): T {
+    val navGraphRoute = destination.parent?.route ?: return hiltViewModel()
+    val parentEntry = remember(this) {
+        navController.getBackStackEntry(navGraphRoute)
+    }
+    return hiltViewModel(parentEntry)
 }
 
--- app/src/main/java/com/mahmoud/tajaarandroid/presentation/util/Route.kt
+++ app/src/main/java/com/mahmoud/tajaarandroid/presentation/util/Route.kt
@@ -1,10 +1,16 @@
 package com.mahmoud.tajaarandroid.presentation.util
 
 object Route {
+
+    // Onboarding graph
+    const val ONBOARDING_GRAPH = "onboarding"
     const val SPLASH = "Splash"
     const val ONBOARDING_STEP_1 = "Onboarding1"
     const val ONBOARDING_STEP_2 = "Onboarding2"
     const val ONBOARDING_STEP_3 = "Onboarding3"
+
+    // Authentication graph
+    const val AUTH = "auth"
     const val GET_STARTED = "GetStarted"
     const val LOGIN = "login"
     const val SIGN_UP = "signup"
@@ -12,6 +18,7 @@ object Route {
     const val FORGOT_PASSWORD = "forgotPassword"
     const val VERIFICATION_CODE = "verification"
 
+    // Main graph.
     const val HOME = "home"
     const val FILTER = "filter"
     const val SEARCH = "search"
