b75f3bc395b98b79454fcef43f7cb2b090bf833d
mahmo <mahmoud.alkateb22@gmail.com>
2023-10-01 15:11:45 +0200

    added buildSrc to organize he dependency management to make it easier to update versions and add or remove dependencies. and also for easy and smoother to add modules in the future

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,18 +55,26 @@ android {
 dependencies {
     val navVersion = "2.7.2"
 
+    //
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
+
+    //
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
 
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    //
     implementation("com.google.accompanist:accompanist-navigation-animation:0.33.1-alpha")
+    //
     testImplementation("junit:junit:4.13.2")
+    //
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    //
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
@@ -76,10 +84,9 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.44")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
     kapt("com.google.dagger:hilt-android-compiler:2.44")
+
+
     implementation("androidx.compose.material:material:1.4.3")
-
-
     kapt("androidx.compose.animation:animation:1.5.1")
-
     implementation("androidx.navigation:navigation-compose:$navVersion")
 }
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,16 @@
+object Versions {
+    const val bomVersion = "2023.03.00"
+
+    const val coreKtxVersion = "1.9.0"
+    const val lifecycleKtxRunTimeVersion = "2.6.2"
+    const val activityComposeVersion = "1.7.2"
+    const val accompanistNavigationAnimationVersion = "0.33.1-alpha"
+    const val junitVersion = "4.13.2"
+    const val junitTestExtVersion = "1.1.5"
+    const val espressoCoreVersion = "3.5.1"
+    const val hiltVersion = "2.44"
+    const val hiltNavVersion = "1.0.0"
+    const val composeMaterialVersion = "1.4.3"
+    const val composeAnimationVersion = "1.5.1"
+    const val composeNavigationVersion = "2.7.2"
+}
