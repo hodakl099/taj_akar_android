56ae789f575a617c61aea74676ca8739ecfae877
mahmo <mahmoud.alkateb22@gmail.com>
2023-10-01 21:22:41 +0200

    removed buildSrc.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,15 +59,12 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
-
     //
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-
     //
     implementation("com.google.accompanist:accompanist-navigation-animation:0.33.1-alpha")
     //
